115b496b140ae3751dec3607ca9c495397d151b6
Aarsh <aarsh@Aarshs-MBP.fios-router.home>
2017-11-19 14:40:33 -0500

    Changed load_data

--- utils/load_data.py
+++ utils/load_data.py
@@ -1,5 +1,7 @@
 import os
 from preprocess_tweets import *
+from sklearn.model_selection import train_test_split
+from random import shuffle
 
 def load_tweets_for_emotion(loc, emotion):
 	""" Load tweets and scores for a specific emotion """
@@ -13,14 +15,23 @@ def load_tweets_for_emotion(loc, emotion):
 			tweets.append((tweet, emotion, score))
 	return tweets
 
-def load_training_data(loc):
-	""" Load all of the training data """
+def load_training_testing_data(loc):
+	""" 
+	Load all of the training and testing data 
+	Returns a 80-20 training/testing split
+	"""
+
 	training_data = []
+	testing_data = []
+	
 	for filename in os.listdir(loc):
 		if filename.endswith(".txt"):
 			emotion = filename.rstrip(".txt")
 			file_location = loc + "/" + filename
 			tweets_for_emotion = load_tweets_for_emotion(file_location, emotion)
+			# shuffle the tweets for the emotion
+			shuffle(tweets_for_emotion)
+
 			training_data.extend(tweets_for_emotion)
 	return training_data
 
