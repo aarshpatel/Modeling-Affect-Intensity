44528036d8fb879a05498fa7ab95001d14bf256b
Lynn Samson <lynnsamson@Lynns-MacBook-Pro.local>
2017-11-19 22:17:40 -0500

    Add encoded emotion to feature vectors

--- evaluate.py
+++ evaluate.py
@@ -16,12 +16,14 @@ def load_model(model_name, X_train, y_train, X_test, y_test, optimization_parame
     return model
 
 
-def get_labels(data):
+def get_emotions_and_labels(data):
     """ Get the labels from the data """
+    emotions = []
     labels = []
     for tweet, emotion, score in data:
+        emotions.append(emotion)
         labels.append(score)
-    return labels
+    return emotions, labels
 
 
 if __name__ == "__main__":
@@ -35,28 +37,51 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     train_input = utils.load_data.load_training_data("data/train")
-    dev_input = utils.load_data.load_training_data("data/dev")
+    # dev_input = utils.load_data.load_training_data("data/dev")
 
-    train_labels = get_labels(train_input)
-    dev_labels = get_labels(dev_input)
+    train_emotions, train_labels = get_emotions_and_labels(train_input)
+    # dev_emotions, dev_labels = get_emotions_and_labels(dev_input)
 
-    train_input.extend(dev_input)
-    train_labels.extend(dev_labels)
+    # train_input.extend(dev_input)
+    # train_labels.extend(dev_labels)
+    # train_emotions.extend(dev_emotions)
 
-    X_train, X_test, y_train, y_test = train_test_split(train_input, train_labels, test_size=0.2, random_state=10)
+    # X_train, X_test, y_train, y_test = train_test_split(train_input, train_labels, test_size=0.2, random_state=10)
 
-    y_train = np.asarray(y_train)
-    y_test = np.asarray(y_test)
+    # y_train = np.asarray(y_train)
+    # y_test = np.asarray(y_test)
+
+    emotion_to_feature = {
+        "anger": 0,
+        "fear": 1,
+        "joy": 2,
+        "sadness": 3
+    }
+
+    train_emotions = [emotion_to_feature[emotion] for emotion in train_emotions]
+
+    train_emotions = np.asarray(train_emotions)
+
+    test_emotions = train_emotions
+
+    print train_emotions.shape[0]
+
+    X_train = train_input
+    y_train = train_labels
+    y_test = y_train
 
     X_train_corpus = [" ".join(tweet) for tweet, emotion, score in X_train]
-    X_test_corpus = [" ".join(tweet) for tweet, emotion, score in X_test]
+    X_test_corpus = [" ".join(tweet) for tweet, emotion, score in X_train]
 
     if args.features == "bow":
-        X_train, X_test = utils.generate_features.generate_bow_features(X_train_corpus, X_test_corpus)
+        X_train, X_test = utils.generate_features.generate_bow_features(X_train_corpus, X_test_corpus, train_emotions, test_emotions)
     elif args.features == "tfidf":
-        X_train, X_test = utils.generate_features.generate_tfidf_features(X_train_corpus, X_test_corpus)
+        X_train, X_test = utils.generate_features.generate_tfidf_features(X_train_corpus, X_test_corpus, train_emotions, test_emotions)
     else:
         X_train, X_test = None, None
+
+    print type(X_train)
+    print type(X_test)
 
     optimization_parameters = {}
     model = load_model(args.model, X_train, y_train, X_test, y_test, optimization_parameters)
--- utils/generate_features.py
+++ utils/generate_features.py
@@ -1,22 +1,56 @@
 """ Generates features for the models """
 
 from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
+import numpy as np
+from scipy import sparse
 
 
-def generate_bow_features(X_train_samples, X_test_samples):
+def generate_bow_features(X_train_samples, X_test_samples, train_emotions, test_emotions):
     """ Generate the bow features for the X_train, X_test """
 
     print("Using BoW features")
     vectorizer = CountVectorizer()
     X_train = vectorizer.fit_transform(X_train_samples)
+    X_train_dense = X_train.todense()
+    X_train_new = np.empty((X_train_dense.shape[1]+1))
+    for idx, train_pt in enumerate(X_train_dense):
+        train_pt_list = train_pt.tolist()[0]
+        train_pt_list.append(train_emotions[idx])
+        np.append(X_train_new, np.asarray(train_pt_list), axis=0)
+    X_train = sparse.csr_matrix(X_train_dense)
+
     X_test = vectorizer.transform(X_test_samples)
+    X_test_dense = X_test.todense()
+    X_test_new = np.empty((X_test_dense.shape[1] + 1))
+    for idx, test_pt in enumerate(X_test_dense):
+        test_pt_list = test_pt.tolist()
+        test_pt_list.append(test_emotions[idx])
+        np.append(X_test_new, np.asarray(test_pt_list), axis=0)
+    X_test = sparse.csr_matrix(X_test_dense)
+
     return (X_train, X_test)
 
 
-def generate_tfidf_features(X_train_samples, X_test_samples):
+def generate_tfidf_features(X_train_samples, X_test_samples, train_emotions, test_emotions):
     """ Generate tfidf features """
     print("Using Tf-idf features")
     vectorizer = TfidfVectorizer()
     X_train = vectorizer.fit_transform(X_train_samples)
+    X_train_dense = X_train.todense()
+    X_train_new = np.empty((X_train_dense.shape[1] + 1))
+    for idx, train_pt in enumerate(X_train_dense):
+        train_pt_list = train_pt.tolist()[0]
+        train_pt_list.append(train_emotions[idx])
+        np.append(X_train_new, np.asarray(train_pt_list), axis=0)
+    X_train = sparse.csr_matrix(X_train_dense)
+
     X_test = vectorizer.transform(X_test_samples)
+    X_test_dense = X_test.todense()
+    X_test_new = np.empty((X_test_dense.shape[1] + 1))
+    for idx, test_pt in enumerate(X_test_dense):
+        test_pt_list = test_pt.tolist()
+        test_pt_list.append(test_emotions[idx])
+        np.append(X_test_new, np.asarray(test_pt_list), axis=0)
+    X_test = sparse.csr_matrix(X_test_dense)
+
     return (X_train, X_test)
