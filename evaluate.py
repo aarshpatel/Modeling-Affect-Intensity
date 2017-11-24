ee6dabae5b738d9f20fc5d47f815f5a57d2e2a13
Aarsh <aarsh@Aarshs-MBP.fios-router.home>
2017-11-24 11:29:53 -0500

    Changed some evaluation things

--- evaluate.py
+++ evaluate.py
@@ -7,12 +7,13 @@ import utils
 import numpy as np
 from sklearn.model_selection import train_test_split
 from tqdm import tqdm
+from sklearn.svm import SVR
+from sklearn.ensemble import RandomForestRegressor
 
-
-def load_model(model_name, X_train, y_train, X_test, y_test, optimization_parameters):
+def load_model(model_name, X_train, y_train, X_test, y_test, optimization_parameters, sklearn_model):
     """ Loads the model with the correct parameters """
     model_source = imp.load_source(model_name, 'models/%s.py' % (model_name))
-    model = model_source.Model(X_train, y_train, X_test, y_test, optimization_parameters)
+    model = model_source.Model(X_train, y_train, X_test, y_test, optimization_parameters, sklearn_model)
     return model
 
 def get_labels(data):
@@ -23,13 +24,13 @@ def get_labels(data):
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("-m", "--model", help="the model to evaluate data on")
-    parser.add_argument("-f", "--features", help="the type of features to use for the model")
+    parser.add_argument("-f", "--features", nargs="*", help="the type of features to use for the model")
     parser.add_argument("-ems", "--metrics", nargs="*", help="the metrics to evaluate the model")
     parser.add_argument('-opt', "--optimize", help="whether or not to optimize the model")
     parser.add_argument("-sv" "--save", help="whether or not to save the model")
 
     args = parser.parse_args()
-
+    print(args)
     # training, testing data is a dictionary... {anger => tweets, fear => tweets, joy => tweets, sadness => tweets}
     training_data, testing_data = utils.load_data.load_training_testing_data("data/train", "data/dev")
 
@@ -38,7 +39,9 @@ if __name__ == "__main__":
     print("Fear:", len(training_data["fear"]))
     print("Joy: ", len(training_data["joy"]))
     print("Sadness: ", len(training_data["sadness"]))
+
     print('\n')
+
     print("Test Data Statistics...\n")
     print("Anger: ", len(testing_data["anger"]))
     print("Fear: ", len(testing_data["fear"]))
@@ -52,27 +55,32 @@ if __name__ == "__main__":
     train_corpus = {emotion: [" ".join(val[0]) for val in values]for emotion, values in training_data.iteritems()}
     test_corpus = {emotion: [" ".join(val[0]) for val in values]for emotion, values in testing_data.iteritems()}
 
+    featurizer = utils.generate_features.Featurizer(args.features, train_corpus, test_corpus)
+    X_train, X_test = featurizer.generate_all_features()
 
-    if args.features == "bow":
-        X_train, X_test = utils.generate_features.generate_bow_features(train_corpus, test_corpus)
+    print X_train
 
-    elif args.features == "tfidf":
-        X_train, X_test = utils.generate_features.generate_tfidf_features(train_corpus, test_corpus)
+    optimization_parameters = {
+        'RandomForestRegresssor': {
+            'n_estimators': [10, 20, 30, 50, 100, 500]
+        },
+        'SVR': {'C': [0.001, 0.01, 0.1, 1, 10]}
+    }
 
-    elif args.features == "lexicons":
-        X_train, X_test = utils.generate_features.generate_lexical_features(train_corpus, test_corpus) 
+    # load the model
+    model = load_model(args.model, X_train, y_train, X_test, y_test, optimization_parameters["SVR"], SVR)
+
+    if args.optimize == 'True':
+        best_model_params = model.optimize("pearson_correlation") # which metric to optimize models
+        print(best_model_params)
+        model.train(best_model_params)
     else:
-        X_train, X_test = None, None
+        model.train()
 
-    print(X_train["anger"].shape)
-    print(X_test["anger"].shape)
-
-    optimization_parameters = {}
-    model = load_model(args.model, X_train, y_train, X_test, y_test, optimization_parameters)
-    model.train()
     for metric in args.metrics:
         predictions = model.predict()
         eval_score, avg_eval = model.evaluate(predictions, y_test, metric)
+        print("\n")
         for emotion, score in eval_score.iteritems():
             print emotion, score 
         print("AVG {0}: {1}".format(metric, avg_eval))
